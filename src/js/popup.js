65ed871c27442265b6b860e5350a3ff07b82f5ea
Gil Fewster <hello@gilfewster.com>
2019-09-11 12:54:05 +1000

    some styles added; using <details> tag for accordian content

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,11 +1,17 @@
-const links = Array.from(document.querySelectorAll('.links a'));
-
-links.map(link => {
-  link.addEventListener('click', function(evt) {
-    navigateToURL(evt.target.href);
+// Security model prvents browser extensions
+// from using anchor tags to link directly to
+// external web pages. You have to use the browser API instead.
+function wrapLinks() {
+  const links = Array.from(document.querySelectorAll('[data-browser-link]'));
+  links.map(link => {
+    link.addEventListener('click', function(evt) {
+      navigateToURL(evt.target.href);
+    });
   });
-});
+}
 
 function navigateToURL(url) {
   chrome.tabs.create({ active: true, url });
 }
+
+wrapLinks();
