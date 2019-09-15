4c137ab8c23a8c98470c335fd5a9ff08f19b9f46
Gil Fewster <hello@gilfewster.com>
2019-09-15 11:43:29 +1000

    Moved the old non-svelte html code for links and offices pages into src/Components dir, ready to be converted into Svelte componens.

--- src/main.js
+++ src/main.js
@@ -8,4 +8,23 @@ const app = new App({
   },
 });
 
+// Security model prvents browser extensions
+// from using anchor tags to link directly to
+// external web pages. You have to use the browser API instead.
+// ToDo: Update WrapLinks to a more Svelte-y approach
+// function wrapLinks() {
+//   const links = Array.from(document.querySelectorAll('[data-browser-link]'));
+//   links.map(link => {
+//     link.addEventListener('click', function(evt) {
+//       navigateToURL(evt.target.href);
+//     });
+//   });
+// }
+
+// function navigateToURL(url) {
+//   chrome.tabs.create({ active: true, url });
+// }
+
+// wrapLinks();
+
 export default app;
